80999b2aea6cb4d895a18f8b338447e15c6b82fc
Mohamedboulj <med.blj93@gmail.com>
2022-03-19 14:20:23 +0100

    Update

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 const text = document.querySelector('p');
 const body = document.querySelector('body');
 var btn = document.querySelector('input');
+var switchBtn = document.querySelector('.switch');
 var d = new Date ;
 var h = d.getHours();
-
-
-//  Declenchement du mode sombre avec le click 
-btn.addEventListener('click',()=>{
-    
+// fonction qui switch vers mode sombre 
+function darkModeToggler(){
     text.classList.toggle('darkp');
-    body.classList.toggle('darkbody')
-})
+    body.classList.toggle('darkbody');
+    switchBtn.classList.toggle('switch_bg');
+    
+}
+//  Declenchement du mode sombre avec le click 
+btn.addEventListener('click',darkModeToggler);
+
 // Passage automatique vers le mode sombre entre 18 P.M et 6 A.M
 if (h > 18 || h < 6) {
-    
-    text.classList.toggle('darkp');
-    body.classList.toggle('darkbody')
+    darkModeToggler();
+     
 }
